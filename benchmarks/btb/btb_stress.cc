4d5e0460df77ff0909654530b9aaadd522982d9d
David Schall <david.schall@tum.de>
2025-05-19 09:19:28 +0000

    Add BTB benchmarks

---
+++ benchmarks/btb/btb_stress.cc
@@ -0,0 +1,121 @@
+/*
+ * Copyright (c) 2025 Technical University of Munich
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met: redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer;
+ * redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution;
+ * neither the name of the copyright holders nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/**
+ * @file
+ * Simple loop benchmark as a template for other benchmarks.
+ * This benchmark is a simple loop that runs for a specified number of
+ * iterations.
+ */
+
+#include <iostream>
+#include <functional>
+#include <map>
+#include <string>
+#include <cassert>
+
+
+#include "benchmarks/base.hh"
+#include "benchmarks/registry.hh"
+
+#include "btb_stress_gen.h"
+
+
+
+class BTBStress : public BaseBenchmark {
+ private:
+  int loop_count;
+  std::string num_branches_str;
+  int num_branches;
+  int br_executed;
+
+ public:
+ BTBStress(std::string name)
+      : BaseBenchmark(name),
+        loop_count(1),
+        num_branches_str("2"),
+        num_branches(2),
+        br_executed(0)
+  {
+  }
+
+  ~BTBStress() {}
+
+  bool init(YAML::Node &bm_config) override {
+    std::cout << "Setup " << _name << std::endl;
+    if (bm_config["num_branches"]) {
+      num_branches_str = bm_config["num_branches"].as<std::string>();
+    }
+    if (bm_config["loop_count"]) {
+      loop_count = bm_config["loop_count"].as<int>();
+    }
+
+    if (num_branches_str.empty()) {
+      std::cerr << "Error: num_branches is not specified." << std::endl;
+      return false;
+    }
+
+    // Check if the last character is 'K' or 'M'
+    if (num_branches_str.back() == 'K') {
+      num_branches = std::stoi(num_branches_str.substr(0, num_branches_str.size() - 1)) * 1024;
+    } else if (num_branches_str.back() == 'M') {
+      num_branches = std::stoi(num_branches_str.substr(0, num_branches_str.size() - 1)) * 1024 * 1024;
+    } else {
+      num_branches = std::stoi(num_branches_str);
+    }
+    if (num_branches <= 0) {
+      std::cerr << "Error: num_branches must be a positive integer." << std::endl;
+      return false;
+    }
+    if (num_branches > MAX_BRANCHES) {
+      std::cerr << "Error: num_branches is too large. MAX:" << MAX_BRANCHES << std::endl;
+      return false;
+    }
+    return true;
+  }
+
+  // Execute the benchmark
+  void exec() override {
+    for (int i = 0; i < loop_count; i++) {
+      br_executed += scramble_btb(num_branches);
+    }
+  }
+
+  void repeat() override {
+  }
+
+  void report() override {
+    std::cout << "Executed BTB scramble function with " << num_branches
+              << " unique branches" << std::endl;
+    std::cout << "Loop count: " << loop_count << std::endl;
+    std::cout << "Total branch executed: " << br_executed << std::endl;
+  }
+};
+
+
+REGISTER_BENCHMARK("btb-stress", BTBStress);
---
+++ benchmarks/btb/btb_stress_gen.py
@@ -0,0 +1,41 @@
+import sys
+
+
+_K=1024
+MAX_BRANCHES=8*_K
+
+
+
+main_tmpl = """
+
+#define MAX_BRANCHES {MAX_BRANCHES}
+
+int __attribute__((optimize("O0"))) scramble_btb(int nbranches) {{
+    int i = 0;
+
+{loop_tmpl}
+
+end:
+    return i;
+}}
+
+"""
+
+# Generate N branches that all check if i is less than `nbranches`
+# After that many unique branches, the function will return
+snippet = """
+    if (i > nbranches) {
+        i = i - 1;
+        goto end;
+    } else {
+        i = i + 1;
+    }
+"""
+
+loop_tmpl = "".join([snippet for _ in range(MAX_BRANCHES)])
+
+
+file_name = sys.argv[1] if len(sys.argv) > 1 else 'btb_stress_asm_gen.h'
+
+with open(file_name, 'w') as f:
+    f.write(main_tmpl.format(MAX_BRANCHES=MAX_BRANCHES, loop_tmpl=loop_tmpl))
